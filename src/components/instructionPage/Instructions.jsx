b60bbe1f99d96506e6e2567fa32170f9e8a13660
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2025-02-12 13:58:21 +0530

    Instructions page done but not responsive will do it later

--- src/components/instructionPage/Instructions.jsx
+++ src/components/instructionPage/Instructions.jsx
@@ -1,5 +1,7 @@
-import { CircleAlert, CircleCheck, Clock3, GraduationCap, MonitorCheck, TriangleAlert, Trophy } from 'lucide-react';
+import { CircleAlert, CircleCheck, CircleHelp, Clock3, GraduationCap, MonitorCheck, TriangleAlert, Trophy } from 'lucide-react';
 import React from 'react';
+import { Checkbox } from "@/components/ui/checkbox"
+import { Button } from '../ui/button';
 
 function Instructions() {
     return (
@@ -16,7 +18,7 @@ function Instructions() {
                 </header>
 
                 {/* ALERT-RED */}
-                <aside className="w-full border border-red-500 rounded-lg mt-5 bg-red-600 bg-opacity-25 p-6">
+                <aside className="w-full border border-red-500 rounded-lg mt-10 bg-red-600 bg-opacity-25 p-6">
                     <div className="flex items-center gap-3">
                         <TriangleAlert size={30} color="#c81414" />
                         <p className="text-xl font-semibold text-red-500">Important Contest Rules</p>
@@ -30,7 +32,7 @@ function Instructions() {
                 </aside>
 
                 {/* Contest Format & Technical Requirements */}
-                <div className="flex flex-col md:flex-row gap-5 mt-5">
+                <div className="flex flex-col md:flex-row gap-10 mt-10">
                     <section className="w-full border border-black rounded-lg bg-[#121B38] p-6">
                         <p className="text-xl font-semibold mb-3">Contest Format</p>
                         <div className=" space-y-2">
@@ -51,7 +53,7 @@ function Instructions() {
                         </div>
                     </section>
                 </div>
-                <div className=" mt-5 flex">
+                <div className=" mt-10 flex">
                     <section className="w-full border border-black  rounded-lg bg-[#121B38] p-6 flex gap-[26%]">
                         <div className="">
                             <p className="text-xl font-semibold  flex gap-2 items-center"><CircleAlert size={25} color="#2563EB" />Rules and Guidelines</p>
@@ -74,17 +76,50 @@ function Instructions() {
                     </section>
                 </div>
 
-                <section className="w-full border mt-5 border-black  rounded-lg bg-[#121B38] p-6">
-                    <p className="text-xl font-semibold mb-3 flex items-center gap-2"><MonitorCheck size={28} color="#2563EB" />Technical Requirement</p>
-                    <div className=" space-y-2">
-                        <p className="flex items-center gap-2"><CircleCheck size={20} color="#2563EB" strokeWidth={1.75} />Modern browser (Chrome 90+, Firefox 88+, Edge 90+).</p>
-                        <p className="flex items-center gap-2"><CircleCheck size={20} color="#2563EB" strokeWidth={1.75} /> Stable internet connection (minimum 1 Mbps).</p>
-                        <p className="flex items-center gap-2"><CircleCheck size={20} color="#2563EB" strokeWidth={1.75} /> Screen resolution: 1280×720 or higher.</p>
-                        <p className="flex items-center gap-2"><CircleCheck size={20} color="#2563EB" strokeWidth={1.75} /> JavaScript enabled in browser settings.</p>
+                <section className="w-full border mt-10 border-black  rounded-lg bg-[#121B38] p-6">
+                    <p className="text-xl font-semibold mb-3 flex items-center gap-2"><CircleHelp color="#2563EB" />Frequently Asked Questions</p>
+                    <div className="flex gap-[9.2%]">
+                        <div className=" space-y-4">
+                            <div className="">
+                                <p className='text-[#2563EB]'>What happens if I lose internet connection?</p>
+                                <p>Your prosgress is automatically saved. You can resume once reconnected.</p>
+                            </div>
+                            <div className="">
+                                <p className='text-[#2563EB]'>How is the scoring calculated?</p>
+                                <p>Your progress is automatically saved. You can resume once reconnected.</p>
+                            </div>
+                        </div>
+
+                        <div className=" space-y-4">
+                            <div className="">
+                                <p className='text-[#2563EB]'>Can I use multiple programming languages?</p>
+                                <p>No, you cannot switch between  languages for different problems.</p>
+                            </div>
+                            <div className="">
+                                <p className='text-[#2563EB]'>What if I face technical issues?</p>
+                                <p>Contact support immediately.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section  >
+
+                <section className='mt-8 flex justify-center'>
+                    <div className="flex items-center space-x-2 ">
+                        <input className='h-10 w-5' type='checkbox' id="terms" />
+                        <label
+                            htmlFor="terms"
+                            className="text-2xl  leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            I have read and agree to the contest rules and guidelines
+                        </label>
                     </div>
                 </section>
+                <div className="flex justify-center mt-8">
+                    <Button variant="blue" size="lg">Start Contest</Button>
+                </div>
+                <div className="h-[10rem]"></div>
             </div>
-        </div>
+        </div >
     );
 }
 
